d17b427117adf1772e6e780720ba9736a3a12964
Chad Zawistowski <chad.zawistowski@gmail.com>
2013-04-02 14:56:35 -0400

    Add ExampleProject

---
+++ src/ExampleProject/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleProject
+{
+    public class Example
+    {
+        public static int EntryPoint(string pwzArgument)
+        {
+            return 0;
+        }
+    }
+}
